2ea50c0cf0cc3747cba1dbc23730b7586cec6b2b
Besnik Br <besnik.br@googlemail.com>
2012-10-19 22:34:29 +0200

    added regex patterns for parsing css

---
+++ src/Parser.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the css-reducer
+ *
+ * (c) Besnik Brahimi <besnik.br@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CssReducer;
+
+/**
+ *
+ * @author Besnik Brahimi <besnik.br@gmail.com>
+ */
+class Parser
+{
+    /**
+     *
+     * @var string
+     */
+    protected $pattern = "~([^{]*)\{([^}]*+)\}~ms";
+
+    /**
+     *
+     * @var string
+     */
+    protected $propertiesPattern = "~([^ :;]*):([^;]*)[;]?~ms";
+}
