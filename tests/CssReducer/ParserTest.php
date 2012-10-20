30bd3a28b22ba4ffcecd9c7e63743161f0f02ae2
Besnik Br <besnik.br@googlemail.com>
2012-10-20 12:53:30 +0200

    added test for minifing css

--- tests/CssReducer/ParserTest.php
+++ tests/CssReducer/ParserTest.php
@@ -35,4 +35,72 @@ class ParserTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(true, array_key_exists('color', $parsedCss[0]['body']));
         $this->assertEquals('#ffffff', $parsedCss[0]['body']['background']);
     }
+
+    protected function getUnminifiedCss()
+    {
+        return "body   ,        p { \n
+                background: #ffffff  ;
+                color: red  ; \t
+                \tfont: Arial\n
+            } ";
+    }
+
+    public function testMinifingCssRemoveTabs()
+    {
+        $css = $this->getUnminifiedCss();
+
+        $p = new Parser();
+
+        $this->assertFalse(strpos($p->minify($css, array(
+            'remove_tabs' => true
+        )), "\t"));
+    }
+
+    public function testMinifingCssRemoveNewlines()
+    {
+        $css = $this->getUnminifiedCss();
+
+        $p = new Parser();
+
+        $this->assertFalse(strpos($p->minify($css, array(
+            'remove_newlines' => true
+        )), "\n"));
+    }
+
+    public function testMinifingCssRemoveWhitespaces()
+    {
+        $css = $this->getUnminifiedCss();
+
+        $p = new Parser();
+        $p->setOption('remove_whitespaces', true);
+        $minifiedCss = $p->minify($css, array(
+            'remove_tabs' => true
+        ));
+
+        $chars = array(' ', ',', ';', ':', '{', '}');
+
+        foreach ($chars as $char)
+        {
+            $charWithLeadingWhitespace = ' '.$char;
+            $charWithTrailingWhitespace = $char.' ';
+
+            $this->assertFalse(strpos($minifiedCss, $charWithLeadingWhitespace));
+            $this->assertFalse(strpos($minifiedCss, $charWithTrailingWhitespace));
+        }
+    }
+
+    public function testMinifingCssRemoveComments()
+    {
+        $css = $this->getUnminifiedCss();
+
+        $p = new Parser();
+
+        $this->assertFalse(strpos($p->minify($css, array(
+            'remove_comments' => true
+        )), "/*"));
+
+        $this->assertFalse(strpos($p->minify($css, array(
+            'remove_comments' => true
+        )), "*/"));
+    }
 }
