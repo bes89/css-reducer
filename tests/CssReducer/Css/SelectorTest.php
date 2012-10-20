8e43a3a9fea6d3de8625c2329f656b87b24661ca
Besnik Br <besnik.br@googlemail.com>
2012-10-21 01:35:30 +0200

    added tests for selector class

--- tests/CssReducer/Css/SelectorTest.php
+++ tests/CssReducer/Css/SelectorTest.php
@@ -12,14 +12,59 @@
 namespace CssReducer\Test\Css;
 
 use CssReducer\Css\Selector;
+use CssReducer\Css\Property\Property;
+use CssReducer\Css\Property\Color;
 
 
 class SelectorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSomething()
+    public function testSetAndGetProperty()
     {
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
+        $property = new Property();
+
+        $selector = new Selector();
+        $selector->setName('body');
+        $selector->addProperty($property);
+
+        $this->assertSame($property, reset($selector->getProperties()));
+    }
+
+    public function testBasePropertiesAreNotMerged()
+    {
+        $property1 = new Property();
+        $property2 = new Property();
+
+        $selector = new Selector();
+        $selector->setName('body');
+        $selector->addProperty($property1);
+        $selector->addProperty($property2);
+
+        $this->assertSame(2, count($selector->getProperties()));
+    }
+
+    public function testNoBasePropertiesOfDifferentTypesAreNotMerged()
+    {
+        $property1 = new Property();
+        $property2 = new Color();
+
+        $selector = new Selector();
+        $selector->setName('body');
+        $selector->addProperty($property1);
+        $selector->addProperty($property2);
+
+        $this->assertSame(2, count($selector->getProperties()));
+    }
+
+    public function testNoBasePropertiesOfSameTypeAreMerged()
+    {
+        $property1 = new Color();
+        $property2 = new Color();
+
+        $selector = new Selector();
+        $selector->setName('body');
+        $selector->addProperty($property1);
+        $selector->addProperty($property2);
+
+        $this->assertSame(1, count($selector->getProperties()));
     }
 }
