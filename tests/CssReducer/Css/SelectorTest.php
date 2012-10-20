40615b3687964d731f0c5b6ce2988b71aa350a9c
Besnik Br <besnik.br@googlemail.com>
2012-10-20 14:34:08 +0200

    added selector class with test (incomplete)

---
+++ tests/CssReducer/Css/SelectorTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the css-reducer
+ *
+ * (c) Besnik Brahimi <besnik.br@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CssReducer\Test\Css;
+
+use CssReducer\Css\Selector;
+
+
+class SelectorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
