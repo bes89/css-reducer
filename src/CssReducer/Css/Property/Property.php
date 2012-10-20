3c3c1645c2a329352042d502a7bd3e88d835f92f
Besnik Br <besnik.br@googlemail.com>
2012-10-21 01:29:27 +0200

    implemented parsing and reducing of css in base but it is not really finished. it does nothing more than putting input data into the inputs array

--- src/CssReducer/Css/Property/Property.php
+++ src/CssReducer/Css/Property/Property.php
@@ -11,10 +11,73 @@
 
 namespace CssReducer\Css\Property;
 
+
 class Property
 {
+    /**
+     * @var array
+     */
+    protected $inputs = array();
+
+    /**
+     * @param null|string $name
+     * @param null|string $value
+     */
+    public function __construct($name = null, $value = null)
+    {
+        if ($name !== null && $value !== null)
+        {
+            $this->parse($name, $value);
+        }
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function parse($name, $value)
+    {
+        $isImportant = false;
+
+        if (strpos($value, '!important') !== false)
+        {
+            $value = str_replace('!important', '', $value);
+            $isImportant = true;
+        }
+
+        $this->inputs[] = array(
+            'name' => $name,
+            'value' => $value,
+            'is_important' => $isImportant,
+        );
+    }
+
+    /**
+     * @param Property $newProperty
+     */
     public function merge(Property $newProperty)
     {
+        list($name, $value, $isImportant) = $newProperty->reduce();
 
+        if ($isImportant)
+        {
+            $value .= '!important';
+        }
+
+        $this->parse($name, $value);
+    }
+
+    /**
+     * @throws \Exception
+     * @return array
+     */
+    public function reduce()
+    {
+        if (count($this->inputs) == 0)
+        {
+            throw new \Exception('There are no inputs.');
+        }
+
+        return $this->inputs[0];
     }
 }
