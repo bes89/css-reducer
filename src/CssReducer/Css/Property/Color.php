f87368fe92973d26936adc2760662069b69f7d4f
Besnik Br <besnik.br@googlemail.com>
2012-10-21 01:31:31 +0200

    color is the first css property for which we created the property class

---
+++ src/CssReducer/Css/Property/Color.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the css-reducer
+ *
+ * (c) Besnik Brahimi <besnik.br@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CssReducer\Css\Property;
+
+
+class Color extends Property
+{
+
+}
