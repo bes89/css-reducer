d1d8916256e09fbf97d4b8e265c045ae548424dd
Besnik Br <besnik.br@googlemail.com>
2012-10-20 14:35:22 +0200

    added optimizer class and incomplete test

---
+++ src/CssReducer/Optimizer.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the css-reducer
+ *
+ * (c) Besnik Brahimi <besnik.br@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CssReducer;
+
+/**
+ *
+ */
+class Optimizer
+{
+
+}
---
+++ tests/CssReducer/OptimizerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the css-reducer
+ *
+ * (c) Besnik Brahimi <besnik.br@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CssReducer\Test;
+
+use CssReducer\Optimizer;
+
+
+class OptimizerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+}
