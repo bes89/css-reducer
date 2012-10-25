749c58804130ee9f572f15b9a60a3a7ae72a3c0c
Besnik Br <besnik.br@googlemail.com>
2012-10-25 20:38:17 +0200

    added factory class for generators and a css generator

---
+++ src/CssReducer/Generator/Css.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of the css-reducer
+ *
+ * (c) Besnik Brahimi <besnik.br@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CssReducer\Generator;
+
+
+class Css
+{
+    public function generate(array $optimizedCss)
+    {
+        $css = "";
+
+        foreach ($optimizedCss as $selector)
+        {
+            $css .= sprintf("%s {\n", $selector->getName());
+
+            foreach ($selector->getProperties() as $index => $property)
+            {
+                $propertyData = $property->reduce();
+
+                $css .= sprintf("  %s: %s%s%s\n",
+                    $propertyData['name'],
+                    $propertyData['value'],
+                    $propertyData['isImportant'] ? ' !important' : '',
+                    count($selector->getProperties())-1 > $index ? ';' : ''
+                );
+            }
+
+            $css .= "}\n\n";
+        }
+
+        return $css;
+    }
+}
