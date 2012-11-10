d497961f8684f81edefb0dd806ee0948ec51e48e
Besnik Br <besnik.br@googlemail.com>
2012-11-10 21:15:41 +0100

    added and interface for a logger and an adapter for monolog

---
+++ src/CssReducer/Log/Monolog.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the css-reducer
+ *
+ * (c) Besnik Brahimi <besnik.br@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace CssReducer\Log;
+
+use Monolog\Logger;
+
+
+class Monolog extends Logger implements LoggerInterface
+{
+}
