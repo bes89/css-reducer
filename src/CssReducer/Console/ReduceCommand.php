c47e1559b4f62e595566b765f45a536bd7165185
Besnik Br <besnik.br@googlemail.com>
2014-08-13 22:31:43 +0200

    added file header comments

--- src/CssReducer/Console/ReduceCommand.php
+++ src/CssReducer/Console/ReduceCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the css-reducer
+ *
+ * (c) Besnik Brahimi <besnik.br@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace CssReducer\Console;
 
 use Symfony\Component\Console\Command\Command;
