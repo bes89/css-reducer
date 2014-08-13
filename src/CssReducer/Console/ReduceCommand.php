f06d283bec78a89bfc6e7c5b7f703a5c032c1bb3
Besnik Br <besnik.br@googlemail.com>
2014-08-13 22:11:33 +0200

    implemented reduce command

--- src/CssReducer/Console/ReduceCommand.php
+++ src/CssReducer/Console/ReduceCommand.php
@@ -30,5 +30,23 @@ class ReduceCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $css = file_get_contents($input->getArgument('file'));
+
+        $logger = new \CssReducer\Log\Monolog();
+
+        $manager = new \CssReducer\Manager($logger);
+        $reducedCss = $manager->reduce($css);
+
+        if ($input->hasArgument('save_file')) {
+            file_put_contents($input->hasArgument('save_file'), $reducedCss);
+        }
+
+        $s1 = strlen($css);
+        $s2 = strlen($reducedCss);
+        $s3 = (1 - ($s2 / $s1)) * 100;
+
+        $output->writeln(sprintf('Complete: %s / %s, %.2f%%',
+            $s1, $s2, $s3
+        ));
     }
 }
